6c22024e22508d769f8ed2cf5e841213c9b02e7f
8davxgnu <davxgnu@tamu.edu>
2023-12-21 10:22:48 -0600

    FIXED City Search

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const apiKey = "2013d2ca36b9dd9275f79651ffa6a683";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+/* ~ DC ~ ☆☆☆
+    - apiURL call blueprint = https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+      NOTICE: after 'weateher?' we just attach different elements combined with '&'
+              to create the search which is search by city in this case. 
+              The elements we include in this case are: 
+              - q={city name}
+              - appid={API Key}
+              - units=metric
+*/
+//----------------------------------------------------
+// input link
+const searchBox    = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
 
-async function checkWeather(){
-    const response = await fetch(apiURL + `&appid=${apiKey}`); 
+
+
+//-----------------------------------------------------------------------------------------
+async function checkWeather(city){
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`); 
     var data       = await response.json();
 
 
-    console.log(data);
+    console.log(data); // View console for better picture of data
 
-    document.querySelector(".city").innerHTML     = data.name;
     /*
     Temperature:
     - DC ~ round to one degree
     */
+    document.querySelector(".city").innerHTML     = data.name;
     document.querySelector(".temp").innerHTML     = (Math.round(data.main.temp * 10 / 10)).toFixed(1)  + "°C"; 
     document.querySelector(".humidity").innerHTML = data.main.humidity   + "%";
     document.querySelector(".wind").innerHTML     = data.wind.speed + "km/h";
 
     
 }
-checkWeather();
+searchButton.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
 
 
 
